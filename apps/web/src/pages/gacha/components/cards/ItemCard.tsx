64bbb075c5a4e45cadd2d5f6e3de5d78aa6a4f7e
shimmy1219 <hiromu19991219@gmail.com>
2025-11-02 23:32:46 +0900

    Revert "fix: reposition item status badges"
    This reverts commit 9508e82b72be095a5f93e35a0103c48eae561e5f.

--- apps/web/src/pages/gacha/components/cards/ItemCard.tsx
+++ apps/web/src/pages/gacha/components/cards/ItemCard.tsx
@@ -109,7 +109,10 @@ export const ItemCard = forwardRef<HTMLDivElement, ItemCardProps>(function ItemC
       onMouseDown={onCardMouseDown}
       onContextMenu={onCardContextMenu}
     >
-      <div className="item-card__status-badges">
+      <div
+        className="absolute z-10 flex flex-col items-end gap-2"
+        style={{ top: '-0.3rem', right: '-0.3rem' }}
+      >
         {model.completeTarget ? <span className="badge badge--status badge--status-complete">完走対象</span> : null}
         {model.pickupTarget ? <span className="badge badge--status badge--status-pickup">ピックアップ</span> : null}
         {model.isRiagu ? <span className="badge badge--status badge--status-riagu">リアグ</span> : null}
