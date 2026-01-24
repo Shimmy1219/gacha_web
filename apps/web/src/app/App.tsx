72d97f9773c4a73a87ed73dd14352fe02794365c
Shimmy1219 <hiromu19991219@gmail.com>
2026-01-24 19:11:17 +0900

    軽微な修正

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -274,7 +274,7 @@ export function App(): JSX.Element {
 
   const gachaLayoutProps: Omit<GachaLayoutProps, 'children'> = {
     title: '四遊楽ガチャツール',
-    tagline: 'Integrated Gacha Management Tool',
+    tagline: 'Integrated Gacha Management Tool emergency',
     mainRef,
     isMobile,
     onDrawGacha: handleDrawGacha,
